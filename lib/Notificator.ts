541e9f206f8b01ffe16d9c9eab18f0e703d6ff3f
bernoulliMbuyamba21 <mmbernoulli@gmail.com>
2023-01-20 01:01:41 +0000

    notificator library completed

--- lib/Notificator.ts
+++ lib/Notificator.ts
@@ -23,6 +23,34 @@ class Notificator{
       throw error;
     }
   }
+
+  notify(exchange: string, channel: string, message: string, routing_key: string){
+    if(!this.isInitialized){
+      Logger.warn('Cannot notify. Notificator not intiated')
+      return
+    }
+
+    try {
+      this.pubsub?.publish(exchange, channel, message, routing_key)
+    } catch (error: any) {
+      Logger.error(`Failed to notify - ${error.message}`)
+      throw error
+    }
+  }
+
+  receive(exchange: string, channel: string, messageHandler: any, binding_key: string){
+    if(!this.isInitialized){
+      Logger.warn('Cannot notify. Notificator not intiated')
+      return
+    }
+
+    try {
+      this.pubsub?.subscribe(exchange, channel, messageHandler, binding_key)
+    } catch (error: any) {
+      Logger.error(`Receive message failed - ${error.message}`)
+      throw error
+    }
+  }
 }
 
 export { Notificator };
