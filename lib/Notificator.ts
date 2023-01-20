50dfd1d164b49c54b39e06536de71165cbf6cccb
bernoulliMbuyamba21 <mmbernoulli@gmail.com>
2023-01-20 14:13:27 +0000

    modify notification

--- lib/Notificator.ts
+++ lib/Notificator.ts
@@ -4,10 +4,14 @@ import Logger from './logger'
 class Notificator{
   pubsub: PubSubDriver | undefined
   isInitialized: boolean
+  KEY: string
+  CHANNEL: string
 
   constructor(args: PubSubDriver){
     this.pubsub = args;
     this.isInitialized = false;
+    this.KEY = 'direct.notification'
+    this.CHANNEL = 'channel'
   }
 
   async init() {
@@ -15,7 +19,7 @@ class Notificator{
     try {
       Logger.info('Notification initialization started...')
       await this.pubsub?.connect()
-      await this.pubsub?.createChannel('channel')
+      await this.pubsub?.createChannel(this.CHANNEL)
       this.isInitialized = true
       Logger.info('Notificator initialization completed.')
     } catch (error: any) {
@@ -24,28 +28,28 @@ class Notificator{
     }
   }
 
-  notify(exchange: string, channel: string, message: string, routing_key: string){
+  notify(message: string){
     if(!this.isInitialized){
       Logger.warn('Cannot notify. Notificator not intiated')
       return
     }
 
     try {
-      this.pubsub?.publish(exchange, channel, message, routing_key)
+      this.pubsub?.publish('', this.CHANNEL, message, this.KEY)
     } catch (error: any) {
       Logger.error(`Failed to notify - ${error.message}`)
       throw error
     }
   }
 
-  receive(exchange: string, channel: string, messageHandler: any, binding_key: string){
+  receive(messageHandler: any){
     if(!this.isInitialized){
       Logger.warn('Cannot notify. Notificator not intiated')
       return
     }
 
     try {
-      this.pubsub?.subscribe(exchange, channel, messageHandler, binding_key)
+      this.pubsub?.subscribe('', this.CHANNEL, messageHandler, this.KEY)
     } catch (error: any) {
       Logger.error(`Receive message failed - ${error.message}`)
       throw error
